a6ff36680c1e2bed06d565a02259fff5f0ddaf85
andreistr21 <60103919+andreistr21@users.noreply.github.com>
2021-09-22 21:01:22 +0200

    Help string update

--- resources/Strings.py
+++ resources/Strings.py
@@ -7,7 +7,7 @@ help_str = (
     "   * Search in your web browser (example: 'Find about Abraham Lincoln' or 'What is it bank')\n"
     "   * Open program by the name (example: 'Open Google Chrome')\n"
     "   * Change the assistant speech rate (example: 'Change the assistant speech rate to 200')\n"
-    "   * Change volume (example: 'Set volume to 50' or 'Increase volume')\n"
+    "   * Change volume (example: 'Set volume to 50' or 'Increase/Decrease volume')\n"
     "   * Shutdown or restart PC (example: 'Shutdown/restart computer' or 'Shutdown/restart computer immediately')\n"
 )
 
