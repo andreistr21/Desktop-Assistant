3519de11c650d44daa24abbf1a01d349c7196709
andreistr21 <60103919+andreistr21@users.noreply.github.com>
2021-10-01 00:15:27 +0200

    Fixed bug of different voices on different PCs

--- bin/classes/Voice.py
+++ bin/classes/Voice.py
@@ -3,11 +3,26 @@ import pyttsx3
 
 class Voice(object):
     def __init__(self, speech_rate):
+        required_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0"
         # Constructs a new TTS engine instance
         self.engine = pyttsx3.init()
+        required_index = -1
+
+        # Search for required voice
+        voices_list = [self.engine.getProperty("voices")][0]
+        # noinspection PyTypeChecker
+        for voice_index in range(len(voices_list)):
+            # noinspection PyUnresolvedReferences
+            if self.engine.getProperty("voices")[voice_index].id == required_id:
+                required_index = voice_index
+                break
+
         # noinspection PyUnresolvedReferences
         # Set voice type
-        self.engine.setProperty("voice", self.engine.getProperty("voices")[1].id)
+        self.engine.setProperty(
+            "voice", self.engine.getProperty("voices")[required_index].id
+        )
+
         # Set speed of speech (words per minute)
         self.engine.setProperty("rate", speech_rate)
 
