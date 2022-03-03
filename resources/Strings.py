0c0dc06dc1706d15247f6c92656c52be1fe7624a
andreistr21 <60103919+andreistr21@users.noreply.github.com>
2022-03-03 14:27:43 +0100

    Update strings

--- resources/Strings.py
+++ resources/Strings.py
@@ -1,25 +1,22 @@
-name_of_assistant = "Sergey"
-welcome_str = "Hi, my name is Sergey and I can help you, just say 'Help me'"
+welcome_str = "Hi, I am the Assistant and I can help you, just say 'Help me'"
 
 help_str = (
-    "Hello, my name is Sergey and I am an assistant. I can:\n"
+    "Hello, I am the Assistant. I can:\n"
     "   * Change your keyboard language (example: 'Change/Switch keyboard language')\n"
     "   * Search in your web browser (example: 'Find about Abraham Lincoln' or 'Search for Barack Obama' or 'What is it bank' or 'Who is Andrzej Duda')\n"
     "   * Open program by the name (example: 'Open Google Chrome')\n"
     "   * Close program by the name (example: 'Close Google Chrome')\n"
-    "   * Change the assistant speech rate (example: 'Change the assistant speech rate to 200')\n"
     "   * Change volume (example: 'Set volume to 50' or 'Increase/Decrease volume')\n"
     "   * Shutdown or restart PC (example: 'Shutdown/restart computer' or 'Shutdown/restart computer immediately')\n"
     "   * Control audio (example: 'Stop/Play music/track', 'Next/Previously music/track')\n"
 )
 
 help_str_for_voice_over = (
-    "Hello, my name is Sergey and I am an assistant. I can:\n"
+    "Hello, I am the Assistant. I can:\n"
     "   Change your keyboard language\n"
     "   Search in your web browser\n"
     "   Open program by the name\n"
     "   Close program by the name\n"
-    "   Change the assistant speech rate\n"
     "   Change volume\n"
     "   Shutdown or restart PC\n"
     "   Control audio\n"
